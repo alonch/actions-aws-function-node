5482d0c8068b1eca874c0c46acd6ae61f370f53f
Alonso Suarez <alonso.suarez@cloudloto.com>
2024-06-10 22:31:36 -0700

    set required default env and permission

--- variables.tf
+++ variables.tf
@@ -21,6 +21,9 @@ variable "memory" {
 variable "env" {
   type        = map(string)
   description = "Environment Variables"
+  default = {
+    "CREATED_BY" = "alonch/actions-aws-function-node"
+  }
 }
 variable "architecture" {
   type        = string
@@ -29,6 +32,7 @@ variable "architecture" {
 variable "permissions" {
   type        = map(string)
   description = "service with access level read/write"
+  default = {}
 }
 variable "artifacts" {
   type = string
