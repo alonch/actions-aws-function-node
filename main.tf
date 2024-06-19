41b0570b1ebed8fd003a3ec9963f308066146a47
Alonso Suarez <alonso.suarez@cloudloto.com>
2024-06-18 22:31:23 -0700

    add basic lambda excetion policy

--- main.tf
+++ main.tf
@@ -10,15 +10,15 @@ locals {
     }
   }
 
-  policies = flatten([
-    for service, access_level in var.permissions : local.services[service][access_level]
-  ])
+  policies = flatten(
+    ["arn:aws:iam::aws:policy/AWSLambdaBasicExecutionRole"],
+    [for service, access_level in var.permissions : local.services[service][access_level]])
 
-    # input: 
-    # var.entrypoint-file   = src/index.js 
-    # output: 
-    # local.entrypoint-file = src/index
-    entrypoint-file = replace(var.entrypoint-file, "/.[^.]+$/", "")
+  # input: 
+  # var.entrypoint-file   = src/index.js 
+  # output: 
+  # local.entrypoint-file = src/index
+  entrypoint-file = replace(var.entrypoint-file, "/.[^.]+$/", "")
 }
 
 data "aws_iam_policy_document" "assume_role" {
@@ -35,14 +35,14 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
-  name               = var.name
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  name                = var.name
+  assume_role_policy  = data.aws_iam_policy_document.assume_role.json
   managed_policy_arns = local.policies
 }
 
 data "archive_file" "lambda" {
   type        = "zip"
-  source_dir = var.artifacts
+  source_dir  = var.artifacts
   output_path = "lambda_function_payload.zip"
 }
 
@@ -51,8 +51,8 @@ resource "aws_lambda_function" "this" {
   function_name = var.name
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "${local.entrypoint-file}.${var.entrypoint-function}"
-  architectures = [ var.architecture ]
-  memory_size = var.memory
+  architectures = [var.architecture]
+  memory_size   = var.memory
 
   source_code_hash = data.archive_file.lambda.output_base64sha256
 
@@ -64,7 +64,7 @@ resource "aws_lambda_function" "this" {
 }
 
 resource "aws_lambda_function_url" "public_url" {
-  count = var.allow-public-access ? 1 : 0
+  count              = var.allow-public-access ? 1 : 0
   function_name      = aws_lambda_function.this.function_name
   authorization_type = "NONE"
 }
